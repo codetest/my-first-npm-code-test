c1f0b1e30b995084e4f46256931aa6ac70725dbb
David Zeng <davzen@microsoft.com>
2020-03-28 22:51:33 +0800

    remove install function

--- index.ts
+++ index.ts
@@ -1,12 +1,2 @@
 import ImageComp from "./src/app.vue"
-import Vue from 'vue'
-
-var installed: boolean = false
-function install(){
-    if (installed) return;
-    installed = true
-    Vue.component('ImageComp', ImageComp)
-}
-
-(<any>ImageComp).install = install;
 export default ImageComp;
